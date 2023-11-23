1a74a20f7ad6ff1ab9f493e1f393d6877fa6fe36
nikita <nik.moiseienko@gmail.com>
2023-11-23 14:41:36 +0100

    fix hono types

--- main.ts
+++ main.ts
@@ -1,12 +1,10 @@
 import { Hono } from "https://deno.land/x/hono@v3.4.1/mod.ts";
-import { serveStatic } from "https://deno.land/x/hono/middleware.ts";
+import { serveStatic } from "npm:@hono/node-server/serve-static";
+// import { serveStatic } from "https://deno.land/x/hono@v3.10.1/middleware.ts";
 
 const app = new Hono();
 
-app.use("/static/*", serveStatic({ root: "./" }));
-app.use("/favicon.ico", serveStatic({ path: "./favicon.ico" }));
-app.get("/", (c) => c.text("You can access: /static/hello.txt"));
-app.get("*", serveStatic({ path: "./static/fallback.txt" }));
+app.get("/*", serveStatic({ root: "./static" }));
 
 Deno.serve({
   port: 3000,
