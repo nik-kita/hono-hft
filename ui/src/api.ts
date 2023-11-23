7cb207f8ff1785a74b0be080a8f79324673ffd3a
nikita <nik.moiseienko@gmail.com>
2023-11-23 17:49:01 +0100

    validator

--- ui/src/api.ts
+++ ui/src/api.ts
@@ -1,11 +1,24 @@
 import { Hono } from "hono";
+import { zValidator } from "@hono/zod-validator";
+import { z } from "zod";
 
 export const api = new Hono()
-  .post("/credentials", (c) => {
-    return c.jsonT({
-      ok: "google",
-    });
-  })
+  .post(
+    "/credentials",
+    zValidator(
+      "json",
+      z.object({
+        key: z.string(),
+        secret: z.string(),
+        passphrase: z.string(),
+      }),
+    ),
+    (c) => {
+      return c.jsonT({
+        ok: "google",
+      });
+    },
+  )
   .get("/hello", (c) => {
     return c.jsonT("world");
   });
